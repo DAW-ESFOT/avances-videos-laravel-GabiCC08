fe97258f2417ebd5c00e38b7e44980e03903b46e
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-01-10 23:45:32 -0500

    #17 Relacion N a N

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,11 +27,20 @@ class UsersTableSeeder extends Seeder
         ]);
         // Generar algunos usuarios para nuestra aplicacion
         for($i = 0; $i < 10; $i++) {
-            User::create([
+            $user = User::create([
                 'name'=> $faker->name,
                 'email'=> $faker->email,
                 'password'=> $password,
             ]);
+            $user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ), $faker->numberBetween(1, 3), false
+                )
+            );
         }
     }
 }
