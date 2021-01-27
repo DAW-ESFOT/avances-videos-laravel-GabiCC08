832eb20adc39b830894398a861fe7d4d43b53555
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-01-27 03:36:42 -0500

    Mails

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,39 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-use App\Http\Resources\Category as CategoryResource;
 use App\Http\Resources\CategoryCollection;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    private static $rules=[
-        'name' => 'required',
-    ];
-    private static $message=[
-        'required' => 'El campo :attribute es obligatorio'
-    ];
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return CategoryCollection
+     */
     public function index()
     {
-        return new CategoryCollection(Category::paginate(10));
+        return new CategoryCollection(Category::all());
+
     }
+
+//    /**
+//     * Store a newly created resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function store(Request $request)
+//    {
+//        //
+//    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Category $category
+     * @return \Illuminate\Http\Response
+     */
     public function show(Category $category)
     {
-        return new CategoryResource ($category);
+        //
     }
-    public function store(Request $request){
-        $request->validate(self::$rules, self::$message);
-        $category = Category::create($request->all());
-        return response()->json($category, 201);
-    }
-    public function update(Request $request, Category $category){
-        $request->validate(self::$rules, self::$message);
-        $category->update($request->all());
-        return response()->json($category, 200);
-    }
-    public function delete(Category $category){
-        $category->delete();
-        return response()->json(null, 204);
-    }
+
 }
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,39 +2,79 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
 use App\Comment;
+use App\Article;
 use App\Http\Resources\Comment as CommentResource;
-use App\Http\Resources\CommentCollection;
+use App\Mail\NewComment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
-    private static $rules=[
-        'text' => 'required',
-    ];
-    private static $message=[
-        'required' => 'El campo :attribute es obligatorio'
-    ];
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Article $article
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(Article $article)
     {
-        return response()->json(new CommentCollection($article->comments),200);
+        return response()->json(CommentResource::collection($article->comments->sortByDesc('created_at')), 200);
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Article $article
+     * @param \App\Comment $comment
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show(Article $article, Comment $comment)
     {
-        $comment= $article->comments()->where('id', $comment->id)->firstOrFail();
-        return new CommentResource ($comment);
+        $comment = $article->comments()->where('id', $comment->id)->firstOrFail();
+        return response()->json(new CommentResource($comment), 200);
     }
-    public function store(Request $request, Article $article){
-        $request->validate(self::$rules, self::$message);
-        $comment=$article->comments()->save(new Comment($request->all()));
-        return response()->json($comment, 201);
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Article $article
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request, Article $article)
+    {
+        $request->validate([
+            'text' => 'required|string'
+        ]);
+
+        $comment = $article->comments()->save(new Comment($request->all()));
+
+        Mail::to($article->user)->send(new NewComment($comment));
+
+        return response()->json(new CommentResource($comment), 201);
     }
-    public function update(Request $request, Article $article){
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
         //
     }
-    public function delete(Comment $comment){
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Comment $comment)
+    {
         //
     }
 }
